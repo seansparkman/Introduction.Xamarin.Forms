f80ce92b505694362d16297cf3aeb24798720821
Sean Sparkman <sean.sparkman@outlook.com>
2018-12-05 16:09:19 -0600

    Step 14 - Create Detail View Model

---
+++ NorthDallas.Contacts/NorthDallas.Contacts/ViewModels/ContactDetailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MvvmHelpers;
+using NorthDallas.Contacts.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthDallas.Contacts.ViewModels
+{
+    public class ContactDetailViewModel
+        : BaseViewModel
+    {
+        public ContactDetailViewModel(Result contact)
+        {
+            Contact = contact;
+            Title = $"{contact.name.first} {contact.name.last}";
+        }
+
+        public Result Contact { get; set; }
+    }
+}
