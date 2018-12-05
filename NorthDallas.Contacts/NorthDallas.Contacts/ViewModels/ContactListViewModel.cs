8965551dbb32251bf1210b0aa19d708e87fa6a48
Sean Sparkman <sean.sparkman@outlook.com>
2018-12-05 15:40:39 -0600

    Step 4 - Create a View Model

---
+++ NorthDallas.Contacts/NorthDallas.Contacts/ViewModels/ContactListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthDallas.Contacts.ViewModels
+{
+    public class ContactListViewModel
+        : BaseViewModel
+    {
+        public ContactListViewModel()
+        {
+            Title = "Welcome to the Contact List";
+        }
+    }
+}
